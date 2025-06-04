61427e7a1e3cbf5c58d205561856cdbfaae6fc5a
alexandre-mateos <mateosalexandre95@gmail.com>
2025-06-04 11:05:57 +0200

    récupération des éléments de ma page

---
+++ assets/scripts/script.js
@@ -0,0 +1,13 @@
+let form = document.querySelector("#formualire");
+let cardName = document.querySelector("#nom-carte");
+let cardType = document.querySelector("#type-carte");
+let cardPower = document.querySelector("#pouvoir-carte");
+
+form.addEventListener("submit", (e) => {
+
+    // empêche le rafraichissment automatique de la page
+    e.preventDefault();
+
+    // c'est là que ça démarre
+
+})
